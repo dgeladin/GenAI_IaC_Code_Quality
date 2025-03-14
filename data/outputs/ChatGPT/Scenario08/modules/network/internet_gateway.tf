cb7c557ddfbd90d42e307bca5be24df0be096099
Daniel Geladin <dan@geladin.com>
2025-03-14 12:26:04 -0400

    Missed copying network module from chat

---
+++ data/outputs/ChatGPT/Scenario08/modules/network/internet_gateway.tf
@@ -0,0 +1,29 @@
+# internet_gateway.tf
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+# route_tables.tf
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
---
+++ data/outputs/ChatGPT/Scenario08/modules/network/outputs.tf
@@ -0,0 +1,20 @@
+# outputs.tf
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "load_balancer_sg_id" {
+  description = "Security Group ID for Load Balancer"
+  value       = aws_security_group.load_balancer_sg.id
+}
