d26398f7519097c78769d4020038243aba7d160d
Daniel Geladin <dan@geladin.com>
2025-03-16 09:50:37 -0400

    updating clean script to not accidentally remove work, and added a TODO file

--- scripts/clean_results.sh
+++ scripts/clean_results.sh
@@ -4,6 +4,9 @@ ROOT_DIR=".."
 RESULTS_DIR="$ROOT_DIR/results"
 
 echo "Cleaning Results Directory  ... "
-rm -rf $RESULTS_DIR/*/*/Scenario*
-rm -rf `pwd`/tmp
+# rm -rf $RESULTS_DIR/*/*/Scenario*
+# rm -rf `pwd`/tmp
+#
+echo "... removed deletion after generating results ... "
+echo "... don't want to accidentally delete my work ... "
 
